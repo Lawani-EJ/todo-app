25ae9cdd6e6edd7428ee5a425feed6592b212694
elyonjohn <elyonjohn60@gmail.com>
2024-03-20 12:53:11 +0100

    Form validation Modified formvalidation in the javascript file

---
+++ login.js
@@ -0,0 +1,11 @@
+function auth() {
+    var email = document.getElementById("email").value;
+    var password = document.getElementById("password").value;
+
+    if (email === "admin@gmail.com" && password === "admin123") {
+        window.location.assign("index2.html");
+        alert("Login Successful");
+    } else {
+        alert("Invalid information");
+    }
+}
